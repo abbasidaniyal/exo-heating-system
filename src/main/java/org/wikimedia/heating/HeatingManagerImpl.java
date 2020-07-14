aa614aec67a5bbbcdb29f9b077f1e8f243106951
Daniyal <abbasi.daniyal98@gmail.com>
2020-07-15 04:06:18 +0530

    refactor 1

--- src/main/java/org/wikimedia/heating/HeatingManagerImpl.java
+++ src/main/java/org/wikimedia/heating/HeatingManagerImpl.java
@@ -8,35 +8,30 @@ import java.net.UnknownHostException;
 public class HeatingManagerImpl {
 
     public void manageHeating(String t, String threshold, boolean active) {
-        double dT = new Double(t);
-        double dThreshold = new Double(threshold);
-        if (dT < dThreshold && active) {
-            try {
-                Socket socket = new Socket("heater.home", 9999);
-                OutputStream os = socket.getOutputStream();
-                os.write("on".getBytes());
-                os.flush();
-                os.close();
-                socket.close();
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else if (dT > dThreshold && active) {
-            try {
-                Socket socket = new Socket("heater.home", 9999);
-                OutputStream os = socket.getOutputStream();
-                os.write("off".getBytes());
-                os.flush();
-                os.close();
-                socket.close();
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        double dT = Double.parseDouble(t);
+        double dThreshold = Double.parseDouble(threshold);
+        if (active) {
+            heaterOn(dT < dThreshold);
         }
+    }
+
+    private void heaterOn(boolean turnOn) {
+        try {
+            Socket socket = new Socket("heater.home", 9999);
+            OutputStream os = socket.getOutputStream();
+            if (turnOn)
+                os.write("on".getBytes());
+            else
+                os.write("off".getBytes());
+            os.flush();
+            os.close();
+            socket.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
